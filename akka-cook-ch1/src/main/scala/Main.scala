95c031ab2c59ef546c7a8e1429d9b63f6cca461d
Shawn Cicoria <github@cicoria.com>
2022-07-20 21:27:23 -0400

    iniitial

---
+++ akka-cook-ch1/src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+package com.packt.chapter1
+           Import akka.actor.ActorSystem
+           /**
+             * Created by user
+             */
+           object HelloAkkaActorSystem extends App {
+             val actorSystem = ActorSystem("HelloAkka")
+             println(actorSystem)
+           }
---
+++ akka-cook-ch1/src/main/scala/com.packt.chapter1/HelloAkkaActorSystem.scala
@@ -0,0 +1,12 @@
+package com.packt.chapter1
+
+import akka.actor.ActorSystem
+
+// Import akka.actor.ActorSystem
+/**
+ * Created by user
+ */
+object HelloAkkaActorSystem extends App {
+  val actorSystem = ActorSystem("HelloAkka")
+  println(actorSystem)
+}
